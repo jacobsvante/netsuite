646fdb1e74e71dccf5da92df6b3778a5b05db5f7
Jacob Magnusson <m@jacobian.se>
2018-12-11 21:05:13 +0100

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup_kwargs = dict(
     name='netsuite',
-    version='0.2.1',
+    version='0.2.2',
     description='Wrapper around Netsuite SuiteTalk Web Services',
     packages=['netsuite'],
     include_package_data=True,
