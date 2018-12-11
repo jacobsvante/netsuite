50ba2ef6cf7cd819595aff04238fac139b202137
Jacob Magnusson <m@jacobian.se>
2018-12-11 14:31:11 +0100

    List Python 3.7 as a supported version

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup_kwargs = dict(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
 
